519312bd98f123c1f69af7c7b44d371757f4eab2
ahsan-K <58878640+ahsan-K@users.noreply.github.com>
2023-09-11 17:26:02 +0500

    db changed to cloud db

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,11 +1,24 @@
+// import mysql from "mysql2/promise";
+
+// const db = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "manager",
+//   database: "booking_system",
+//   port: "6000",
+// });
+
+// export default db;
+
+
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "manager",
-  database: "booking_system",
-  port: "6000",
+  host: "sql11.freemysqlhosting.net",
+  user: "sql11645672",
+  password: "ihquMCzLYl",
+  database: "sql11645672",
+  port: "3306",
 });
 
 export default db;
